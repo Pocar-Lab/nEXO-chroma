2e34334706cfceb491303bcbdac3ff0a9778b084
LoickMarion <loick.marion@gmail.com>
2024-07-06 19:01:45 -0400

    assigned default values for n (1000000), r (0) and s (random number). The only required argument now is the experiment name

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys, getopt
+import numpy as np
 
 from analysis.geometry_manager import geometry_manager
 from analysis.run_manager import run_manager
@@ -14,10 +15,10 @@ def usage():
     print ("=====================================================================")
     print ("  The minimum paramaters the simulation needs are:")
     print ("    (1) '-e' <Str>              name of experiment to be simulated.")
+    print ("  Additional options can be chosen:")
     print ("  	(2) '-n' <#>	            number of photons to be simulated.") 
     print ("  	(3) '-s' <#>                choose the seed number")
     print ("  	(4) '-r' <#>                run ID number")
-    print ("  Additional options can be chosen:")
     print ("    (5) '-p' <Str1,Str2,...>    choose which plots to run")
     print ("  	(6) '-v'          to view the setup before simulation.")
     print ("  	(7) '-w'          to save simulation data from the run")
@@ -33,9 +34,9 @@ def main():
         sys.exit()
 
     experiment_name = None
-    num_particles = None
-    seed = None
-    run_id = None
+    num_particles = 1000000
+    seed = np.random.randint(0,1000000)
+    run_id = 1
     visualize = False
     plots = []
     write = False
@@ -60,26 +61,17 @@ def main():
         print("Please input an experiment name")
         usage()
         sys.exit()
-    if not num_particles:
-        print("Please input a number of particles")
-        usage()
-        sys.exit()
-    if not seed:
-        print("Please input a seed")
-        usage()
-        sys.exit()
-    if not run_id:
-        print(" Please input a run id")
-        usage()
-        sys.exit()
 
     print('Experiment Name:         ' + experiment_name)
     print('Number of particles:     ' + str(num_particles))
     print('Random seed:             ' + str(seed))
     print('Run ID:                  ' + str(run_id))
     print('Visualize:               ' + str(visualize))
-    print('Plots:                   ' + ', '.join(plots))
-    print('Saving Data:             ' + str(write) )
+    if(len(plots) > 0):
+        print('Plots:                   ' + ', '.join(plots))
+    else:
+        print('Plots:                   ' + 'None')
+    print('Saving Data:             ' + str(write))
 
 
     gm = geometry_manager(experiment_name=experiment_name, run_id=run_id, visualize=visualize)
