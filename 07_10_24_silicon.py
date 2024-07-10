b2c7540e9d25e4062d0975fef33730ff993fc4a2
lab57 <luc.barrett57@gmail.com>
2024-07-10 13:58:28 -0400

    Analysis script, added exclusion option to geometry manager

---
+++ 07_10_24_silicon.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+from analysis.geometry_manager import geometry_manager
+from analysis.run_manager import run_manager
+import time
+
+
+"""
+Example showing how to run a simulation with a script file rather than from the command line.
+
+Here perhaps include description of what the simulation is testing for archival reasons.
+
+Run this file from within the Chroma container with `python ./ExampleAnalysis.py`
+"""
+
+
+def main():
+
+    experiment_name = "SiliconFlippedSourceLower"
+    num_particles = 1_000_000
+    seed = 1042
+    run_id = 1
+    visualize = True
+    plots = []
+
+    exclusions = [
+        [2, 3, 4, 5, 6, 7, 8, 9],
+        [],
+        [2, 3, 4, 9],
+        [
+            5,
+            6,
+            7,
+            8,
+        ],
+    ]
+
+    labels = [
+        "silicon-noreflector",
+        "silicon-8reflector",
+        "silicon-4reflector-outer",
+        "silicon-4reflector-inner",
+    ]
+
+    exclusions = [[f"reflector{i}" for i in excl] for excl in exclusions]
+    ptes = []
+    ptes_err = []
+    for exclusion in exclusions:
+        print(f"Experiment Name:       {experiment_name }")
+        print(f"Number of particles:   {str(num_particles) }")
+        print(f"Random seed:           {str(seed) }")
+        print(f"Run ID:                {str(run_id) }")
+        print(f"Visualize:             {str(visualize) }")
+        print(f"Plots:                 {plots}")
+        print(f"Excluded reflectors:   {exclusion}")
+
+        gm = geometry_manager(
+            experiment_name=experiment_name,
+            run_id=run_id,
+            visualize=visualize,
+            exclude=exclusion,
+        )
+        rm = run_manager(
+            geometry_manager=gm,
+            experiment_name=experiment_name,
+            random_seed=seed,
+            num_particles=num_particles,
+            run_id=run_id,
+            plots=plots,
+            write=False,
+        )
+        pte = rm.ana_man.photon_transmission_efficiency
+        ptes.append(pte)
+        ptes_err.append(rm.ana_man.pte_st_dev)
+        run_id += 1
+    print("-----------------------------")
+    print("Conclusion")
+    for i in range(len(labels)):
+        print(f"Run{i+1}: {labels[i]: <35} - {ptes[i]} +- {ptes_err[i]}")
+
+
+if __name__ == "__main__":
+    s = time.time()
+    main()
+    e = time.time()
+    print(f"The simulation run time is: {e - s} s")
--- analysis/geometry_manager.py
+++ analysis/geometry_manager.py
@@ -14,40 +14,45 @@ from mpl_toolkits import mplot3d
 
 from .material_manager import material_manager
 from .surface_manager import surface_manager
+
+
 # all components, general geometry, manage surface and material
 class geometry_manager:
     """
-        Manages the geometry of an experiment by reading component data from a CSV file,
-        creating solid objects, and organizing them into a detector geometry.
-        
-        Attributes:
-            experiment_name (str): String used to identify each experiment.
-            run_id (int): Identifier for the run.
-            mat_manager (material_manager): Instance of the material_manager class.
-            surf_manager (surface_manager): Instance of the surface_manager class.
-            global_geometry (Detector): The global detector geometry.
-            geometry_data_path (str): Path to the CSV file with geometry component data.
-            geometry_df (pd.DataFrame): DataFrame containing the geometry component data.
-            solids (dict): Dictionary of solid objects.
-        """
-    def __init__(self, experiment_name, run_id,visualize = False):
+    Manages the geometry of an experiment by reading component data from a CSV file,
+    creating solid objects, and organizing them into a detector geometry.
+
+    Attributes:
+        experiment_name (str): String used to identify each experiment.
+        run_id (int): Identifier for the run.
+        mat_manager (material_manager): Instance of the material_manager class.
+        surf_manager (surface_manager): Instance of the surface_manager class.
+        global_geometry (Detector): The global detector geometry.
+        geometry_data_path (str): Path to the CSV file with geometry component data.
+        geometry_df (pd.DataFrame): DataFrame containing the geometry component data.
+        solids (dict): Dictionary of solid objects.
+    """
+
+    def __init__(self, experiment_name, run_id, visualize=False, exclude=None):
         """
         Initializes the geometry_manager with the given experiment name and run ID.
-        
+
         Args:
             experiment_name (str): String used to identify each experiment.
             run_id (int): Identifier for the run.
             visualize (bool): If True, visualize the geometry.
         """
-
+        self.exclude = [] if exclude is None else exclude
         self.experiment_name = experiment_name
         self.mat_manager = material_manager(self.experiment_name, run_id)
-        self.surf_manager = surface_manager(self.mat_manager, self.experiment_name, run_id)
+        self.surf_manager = surface_manager(
+            self.mat_manager, self.experiment_name, run_id
+        )
         self.run_id = run_id
         self.global_geometry = Detector(self.mat_manager.global_material)
 
-        self.geometry_data_path = f'/workspace/data_files/data/{experiment_name}/geometry_components_{experiment_name}.csv'
-        self.build_geometry()
+        self.geometry_data_path = f"/workspace/data_files/data/{experiment_name}/geometry_components_{experiment_name}.csv"
+        self.build_geometry(exclude=exclude)
 
         self.global_geometry.flatten()
         if visualize:
@@ -59,64 +64,83 @@ class geometry_manager:
         Builds the geometry by reading the CSV file and creating solid objects.
         Adds the solids to the global geometry based on their type.
         """
+
         # read in the csv file into dataframe
         self.geometry_df = pd.read_csv(self.geometry_data_path)
 
         # iterate through all geometries and create Solid object, store into dictionary of solids
         self.solids = {}
         for index, row in self.geometry_df.iterrows():
-            curr_name = row['name']
-            mesh = mesh_from_stl(filename = row['stl_filepath'])#convert the stl files to mesh used in Chroma?
-            inner_mat = self.mat_manager.get_material(row['inner_mat'])
-            outer_mat = self.mat_manager.get_material(row['outer_mat'])
+            curr_name = row["name"]
 
-            if bool(row['killing surface']):
-                surface = self.surf_manager.get_surface('killing surface')
+            if curr_name in self.exclude:
+                continue
+
+            mesh = mesh_from_stl(
+                filename=row["stl_filepath"]
+            )  # convert the stl files to mesh used in Chroma?
+            inner_mat = self.mat_manager.get_material(row["inner_mat"])
+            outer_mat = self.mat_manager.get_material(row["outer_mat"])
+
+            if bool(row["killing surface"]):
+                surface = self.surf_manager.get_surface("killing surface")
             else:
-                surface = self.surf_manager.get_surface(row['surface'])
+                surface = self.surf_manager.get_surface(row["surface"])
 
-            color = int(colors.cnames[row['color']][1:], 16)
+            color = int(colors.cnames[row["color"]][1:], 16)
 
-            curr_displacement = (row['displacement x'], row['displacement y'], row['displacement z'])
-            print(curr_displacement)       
-                
+            curr_displacement = (
+                row["displacement x"],
+                row["displacement y"],
+                row["displacement z"],
+            )
+            print(curr_displacement)
+
             # #below shift the vertical displacement
             # if curr_name == "silica gasket 1" or curr_name == "silica gasket 2" or curr_name == "silica window":
             #     curr_displacement = (row['displacement x'], row['displacement y']+self.run_id*0.01, row['displacement z'])
-            #     print(curr_displacement)   
-            #     # print(type(row['displacement y']))        
+            #     print(curr_displacement)
+            #     # print(type(row['displacement y']))
             # #Solid object attaches materials, surfaces, and colors to each triangle in a Mesh object
             # else:
             #     curr_displacement = (row['displacement x'], row['displacement y'], row['displacement z'])
             #     # print(curr_displacement)
 
-            self.solids[curr_name] =  Solid(mesh = mesh, 
-                                            material1 = inner_mat,
-                                            material2 = outer_mat,
-                                            surface = surface,
-                                            color = color)
+            self.solids[curr_name] = Solid(
+                mesh=mesh,
+                material1=inner_mat,
+                material2=outer_mat,
+                surface=surface,
+                color=color,
+            )
             # check to see if it is a detecting volume, add to geometry, (pmt and other solid will be treated differently?)
-            if row['solid_type'] == 'pmt':
-                self.global_geometry.add_pmt(pmt = self.solids[curr_name],
-                                            rotation = None,
-                                            displacement = curr_displacement)
-            elif row['solid_type'] == 'solid':
-                self.global_geometry.add_solid(solid = self.solids[curr_name],
-                                            rotation = None,
-                                            displacement = curr_displacement)       
+            if row["solid_type"] == "pmt":
+                self.global_geometry.add_pmt(
+                    pmt=self.solids[curr_name],
+                    rotation=None,
+                    displacement=curr_displacement,
+                )
+            elif row["solid_type"] == "solid":
+                self.global_geometry.add_solid(
+                    solid=self.solids[curr_name],
+                    rotation=None,
+                    displacement=curr_displacement,
+                )
 
     def get_solid_center(self, name):
         """
         Gets the center of a solid object.
-        
+
         Args:
             name (str): The name of the solid.
-        
+
         Returns:
             list: Coordinates of the center of the solid.
         """
-        #this solid center is to define the centre of the photon source? Use it later?
+        # this solid center is to define the centre of the photon source? Use it later?
         curr_mesh_triangles = self.solids[name].mesh.get_triangle_centers()
-        return [np.mean(curr_mesh_triangles[:, 0]),
-                np.mean(curr_mesh_triangles[:, 1]),
-                np.mean(curr_mesh_triangles[:, 2])]
+        return [
+            np.mean(curr_mesh_triangles[:, 0]),
+            np.mean(curr_mesh_triangles[:, 1]),
+            np.mean(curr_mesh_triangles[:, 2]),
+        ]
