8356d80837003c3cba40e71b50229744e1b734cb
Ed van Bruggen <ed@edryd.org>
2026-08-10 12:58:53 -0400

    Release v4.0.0

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 
 setup(
     name="PocarChroma",
-    version="2.0",
-    description="Run and analysis tools for Chroma simulations for nEXO",
-    author="Luc Barrett, Loick Marion",
-    author_email="me@lucbarrett.info",
+    version="4.0.0",
+    description="Run and analysis tools for Chroma simulations",
+    author="Edryd van Bruggen, Sam Albury, Luc Barrett, Loick Marion",
+    author_email="ed@edryd.org",
     packages=["PocarChroma"],
     install_requires=["numpy-stl"]
 )
